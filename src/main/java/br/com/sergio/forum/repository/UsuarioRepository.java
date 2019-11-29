60e26fbac9a0d793e5b7e4bfc428e89e0417fe99
Sergio Junior <sergio.ltnj@gmail.com>
2019-11-29 12:12:47 -0300

    Implementado restrição de acesso e metodo de autenticação de usuario

---
+++ src/main/java/br/com/sergio/forum/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.sergio.forum.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.sergio.forum.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	Optional<Usuario> findByEmail(String email);
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$zBir3E8Yf8DJlxyfFsSQ1uthZHawsiR17rUazY8ecqS7lZzmOF172');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
